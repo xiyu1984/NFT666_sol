889cf42630e0392f1f40a431a7409dffc6e85a52
xiyu1984 <shanellyy@126.com>
2022-02-28 14:29:05 +0800

    implementation

---
+++ README.md
@@ -0,0 +1,12 @@
+# Solidity Smart Contract For NFT666
+
+## Description
+Something interesting will be happened in metaverse, but the current standard for non-fungible tokens may not be able to adapt to some interesting situations. For example, if someone wants to lend an exceptionally valuable digital asset in the virtual world, to a friend, the existing standards do not guarantee that his friend will return it.
+
+I believe that in the future metaverse world, the rental of digital assets is a very common situation, so I am ready to study how to provide a better service based on the existing NFT standard.
+
+First, I'll try make NFT666 from ERC721(https://eips.ethereum.org/EIPS/eip-721).
+
+## Getting started
+
+It's under construction...
--- contracts/NFT666.sol
+++ contracts/NFT666.sol
@@ -3,19 +3,80 @@ pragma solidity >=0.4.22 <0.9.0;
 
 import "./INFT666.sol";
 
-contract NFT666 {
+contract NFT666 is INFT666, ERC721Enumerable, ERC721Metadata, ERC721 {
   address public owner = msg.sender;
-  uint public last_completed_migration;
 
-  modifier restricted() {
-    require(
-      msg.sender == owner,
-      "This function is restricted to the contract's owner"
-    );
-    _;
+  function name() external view returns (string memory _name){
+
   }
 
-  function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
+  function symbol() external view returns (string memory _symbol){
+
+  }
+
+  function tokenURI(uint256 _tokenId) external view returns (string memory){
+
+  }
+
+  function totalSupply() external view returns (uint256){
+    
+  }
+
+  function tokenByIndex(uint256 _index) external view returns (uint256){
+
+  }
+
+  function tokenOfOwnerByIndex(address _owner, uint256 _index) external view returns (uint256){
+
+  }
+
+  // ERC721
+  function balanceOf(address _owner) external view returns (uint256){
+
+  }
+
+  function ownerOf(uint256 _tokenId) external view returns (address){
+
+  }
+
+  function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes memory data) external payable{
+
+  }
+
+  function safeTransferFrom(address _from, address _to, uint256 _tokenId) external payable{
+
+  }
+
+  function transferFrom(address _from, address _to, uint256 _tokenId) external payable{
+
+  }
+
+  function approve(address _approved, uint256 _tokenId) external payable{
+
+  }
+
+  function setApprovalForAll(address _operator, bool _approved) external{
+
+  }
+
+  function getApproved(uint256 _tokenId) external view returns (address){
+
+  }
+
+  function isApprovedForAll(address _owner, address _operator) external view returns (bool){
+
+  }
+
+  // INFT666
+  function lendUsageTo(address to, uint256 token_id, uint64 period) external{
+
+  }
+
+  function usageReturn(uint256 token_id) external{
+
+  }
+
+  function getLeasingPeriod(uint256 token_id) external view returns (uint64){
+
   }
 }
